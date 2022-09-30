e1b8d591e20c8167863354857690b2767e671fb0
Shendy <03081200024@student.uph.edu>
2022-09-30 09:30:14 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>SHENDY DENNISON</title>
+
+        <!-- Styles -->
+        <style>
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
+        </style>
+        <!-- Google Fonts -->
+         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
+
+        <link href="/css/main.css" rel="stylesheet">
+        <link href="/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    </head>
+    <body class="antialiased">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <!-- Container wrapper -->
+            <div class="container-fluid">
+              <!-- Toggle button -->
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-mdb-toggle="collapse"
+                data-mdb-target="#navbarCenteredExample"
+                aria-controls="navbarCenteredExample"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i class="fas fa-bars"></i>
+              </button>
+          
+              <!-- Collapsible wrapper -->
+              <div
+                class="collapse navbar-collapse justify-content-center"
+                id="navbarCenteredExample"
+              >
+                <!-- Left links -->
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">HOME</a>
+                  </li>
+         
+                  <!-- Navbar dropdown -->
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">ABOUT ME</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="/resume">RESUME</a></li>
+                      <li><a class="dropdown-item" href="/hasil">PORTFOLIO</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="/contact">CONNECT WITH ME</a></li>
+                    </ul>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/contact">CONTACT</a>
+                    </li>
+                  </li>
+                </ul>
+            </div>
+        </nav>
+        
+        <section id="hero">
+            <div class="hero-container">
+              <h1>Shendy Dennison</h1>
+              <h2>I'm a Professional Digital Marketer In Medan City</h2>
+              <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
+            </div>
+        </section><!-- End Hero -->
+
+         <!-- ======= My Resume Section ======= -->
+         <section id="resume" class="resume">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-6">
+                  <h3 class="resume-title">Ringkasan</h3>
+                  <div class="resume-item pb-0">
+                    <h4>Shendy Dennison</h4>
+                    <p><em>Pribadi yang inovatif dengan pengalaman lebih dari 2 tahun merancang dan mengembangkan materi pemasaran digital yang berpusat pada pengguna dari konsep awal hingga hasil akhir.</em></p>
+                    <p>
+                    <ul>
+                      <li>Cemara No. 88V</li>
+                      <li>+62 853-5897-1128</li>
+                      <li>03081200024@student.uph.edu</li>
+                    </ul>
+                    </p>
+                  </div>
+      
+                  <h3 class="resume-title">Pendidikan</h3>
+                  <div class="resume-item">
+                    <h4>Sarjana Komputer</h4>
+                    <h5>2020 - 2024</h5>
+                    <p><em>Universitas Pelita Harapan Kampus Medan</em></p>
+                    <p>Jurusan Sistem Informasi</p>
+                  </div>
+                  <div class="resume-item">
+                    <h4>Sekolah Menengah Atas</h4>
+                    <h5>2017 - 2020</h5>
+                    <p><em>Yayasan Perguruan Sutomo 1</em></p>
+                    <p>Peminatan IPA</p>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <h3 class="resume-title">Pengalaman Profesional</h3>
+                  <div class="resume-item">
+                    <h4>Majelis Perwakilan Mahasiswa</h4>
+                    <h5>2022 - Present</h5>
+                    <p><em>Universitas Pelita Harapan Kampus Medan</em></p>
+                    <p>
+                    <ul>
+                      <li>Secara langsung aktif sebagai anggota di komisi survei dan data. </li>
+                      <li>Bertanggung jawab terhadap kerahasiaan data mahasiswa yang mengisi survei dan aspirasi.</li>
+                      <li>Sebagai fungsi pengawas terhadap setiap acara yang dilangsungkan oleh Organisasi Kemahasiswaan (OK).</li>
+                    </ul>
+                    </p>
+                  </div>
+                  <div class="resume-item">
+                    <h4>Kepala Departemen Badan Eksekutif Mahasiswa</h4>
+                    <h5>2021 - 2022</h5>
+                    <p><em>Universitas Pelita Harapan Kampus Medan</em></p>
+                    <p>
+                    <ul>
+                      <li>Secara langsung aktif sebagai ketua departemen pengembangan wawasan dan penalaran.</li>
+                      <li>Mengembangkan wawasan mahasiswa/i dengan membahas isu-isu aktual yang relevan dan berguna.</li>
+                      <li>Sebagai lembaha eksekutif berskala universitas untuk meningkatkan kualitas hidup mahasiswa/i.</li>
+                    </ul>
+                    </p>
+                  </div>
+                </div>
+              </div>
+      
+            </div>
+        </section><!-- End My Resume Section -->
+
+         <!-- ======= My Portfolio Section ======= -->
+        <section id="portfolio" class="portfolio">
+          <div class="container">
+            <h2 style="text-align: center;    font-size: 26px;
+            font-weight: 700;
+            margin-top: 50px;
+            margin-bottom: 20px;
+            color: #3b434a;">PORTFOLIO SAYA</h2>
+            <div class="row portfolio-container">
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                <div class="portfolio-img"><img src="/../portfolio/portfolio-1.jpg" class="img-fluid" alt=""></div>
+                </div>
+    
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                <div class="portfolio-img"><img src="/../portfolio/portfolio-2.jpg" class="img-fluid" alt=""></div>
+                </div>
+    
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                <div class="portfolio-img"><img src="/../portfolio/portfolio-3.jpg" class="img-fluid" alt=""></div>
+                </div>
+    
+            </div>
+          </div>
+        </section><!-- End My Portfolio Section -->
+
+        <!-- ======= Contact Me Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 style="text-align: center;    font-size: 26px;
+          font-weight: 700;
+          margin-top: 50px;
+          margin-bottom: 20px;
+          color: #3b434a;">TERHUBUNG DENGAN SAYA</h2>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="info-box">
+                  <i class="bx bx-share-alt"></i>
+                  <h3>Social Profiles</h3>
+                  <div class="social-links">
+                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
+                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-envelope"></i>
+                  <h3>Email Me</h3>
+                  <p>03081200024@student.uph.edu</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-phone-call"></i>
+                  <h3>Call Me</h3>
+                  <p>+62 853-5897-1128</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Me Section -->
+
+      <footer id="footer">
+        <div class="container">
+          <h3>Shendy Dennison</h3>
+          <p>Digital marketing isn't about marketing only</p>
+          <div class="social-links">
+            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+          </div>
+          <div class="copyright">
+            &copy; Copyright <strong><span>Shendy Dennison</span></strong>. All Rights Reserved
+          </div>
+        </div>
+      </footer><!-- End Footer -->
+
+    </body>
+</html>
